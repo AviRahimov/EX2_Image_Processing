e8cac6a890661b9fb40b99df4db84058e522b86a
Avi Rahimov <rahimovavi3@gmail.com>
2023-04-23 22:05:09 +0300

    complete 2 functions

--- ex2_utils.py
+++ ex2_utils.py
@@ -50,6 +50,49 @@ def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     :param kernel: A kernel
     :return: The convolved image
     """
+    # first, we flip the kernel
+    kernel_flipped = np.flip(kernel)
+    # calculate the number of pixels to add for each axis since we need to perform the padding like the
+    # cv2.BORDER_REPLICATE type so I use the same padding method with edge mode so we can save the edges
+    # of rhe original image
+    num_of_edge_padding = kernel_flipped[0]//2
+    img_mat = np.pad(in_image, pad_width=num_of_edge_padding, mode='edge')
+    res_mat = np.zeros(in_image.shape)
+    for i in range(img_mat.shape[0]):
+        for j in range(img_mat.shape[1]):
+            conv_pixel = 0
+            for k in range(kernel_flipped.shape[0]):
+                for l in range(kernel_flipped.shape[1]):
+                    conv_pixel += img_mat[i + k, j + l] * kernel_flipped[k, l]
+            res_mat[i, j] = conv_pixel
+    return res_mat
+
+
+# def conv2D(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+#     # Compute the dimensions of the input image and kernel
+#     rows, cols = image.shape
+#     kernel_rows, kernel_cols = kernel.shape
+#
+#     # Compute the padding needed for valid convolution
+#     pad_height = kernel_rows // 2
+#     pad_width = kernel_cols // 2
+#
+#     # Initialize output matrix
+#     output = np.zeros((rows - 2 * pad_height, cols - 2 * pad_width))
+#
+#     # Flip the kernel
+#     kernel = np.flip(kernel)
+#
+#     # Pad the image with zeros
+#     image_padded = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), 'constant')
+#
+#     # Compute convolution
+#     for i in range(pad_height, rows + pad_height):
+#         for j in range(pad_width, cols + pad_width):
+#             output[i - pad_height, j - pad_width] = np.sum(
+#                 image_padded[i - pad_height:i + pad_height + 1, j - pad_width:j + pad_width + 1] * kernel)
+#
+#     return output
 
 
 def convDerivative(in_image: np.ndarray) -> (np.ndarray, np.ndarray):
@@ -58,12 +101,19 @@ def convDerivative(in_image: np.ndarray) -> (np.ndarray, np.ndarray):
     :param in_image: Grayscale image
     :return: (directions, magnitude)
     """
+    # derivative vectors
+    rows_vec = np.array([1, 0, -1])
+    column_vec = rows_vec.T
 
+    x_derivative = conv2D(in_image, rows_vec)
+    y_derivative = conv2D(in_image, column_vec)
     """
     Magnitude = |∇f| =  √((∂f/∂x)² + (∂f/∂y)²)
     Direction = α = arc-tan((∂f/∂y) / (∂f/∂x))
     """
-
+    magnitude = np.sqrt(np.power(x_derivative, 2) + np.power(y_derivative, 2)).asype(np.float64)
+    direction = np.arctan2(y_derivative/x_derivative).asype(np.float64)
+    return magnitude, direction
 
 def blurImage1(in_image: np.ndarray, k_size: int) -> np.ndarray:
     """
